e3b46e5791b73544aa1bcc535cb0890498ab1d77
ayrton <ayrton.parkinson1@gmail.com>
2023-01-15 14:34:57 -0500

    Create Community model

---
+++ db/migrate/20230115193359_create_communities.rb
@@ -0,0 +1,11 @@
+class CreateCommunities < ActiveRecord::Migration[7.0]
+  def change
+    create_table :communities do |t|
+      t.string :sub_dir
+      t.string :title
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
