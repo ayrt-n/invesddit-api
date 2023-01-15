c465d8703ac30b980b6a94444d02a3195772380f
ayrton <ayrton.parkinson1@gmail.com>
2023-01-15 13:35:28 -0500

    Add login helper

--- spec/support/api_helpers.rb
+++ spec/support/api_helpers.rb
@@ -2,4 +2,13 @@ module ApiHelpers
   def json
     JSON.parse(response.body)
   end
+
+  def login_with_api(account)
+    login_params = {
+      login: account.email,
+      password: account.password
+    }
+
+    post '/login', params: login_params, as: :json
+  end
 end
