f25554c605d0a05dcfed50fe0d396ec732bfbbc1
ayrton <ayrton.parkinson1@gmail.com>
2023-01-15 15:05:01 -0500

    Add #render_resource and #validation_error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 class ApplicationController < ActionController::Base
+  def render_resource(resource)
+    if resource.errors.empty?
+      render json: resource, status: :created
+    else
+      validation_error(resource)
+    end
+  end
+
+  def validation_error(resource)
+    render json: {
+      error: {
+        title: 'Unprocessable Entity',
+        details: resource.errors.full_messages
+      }
+    }, status: :unprocessable_entity
+  end
+
   private
 
   def authenticate
