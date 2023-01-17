35e9127a33accd0dc1d7ddc7157f13f950b9ade6
ayrton <ayrton.parkinson1@gmail.com>
2023-01-17 07:51:27 -0500

    Add test for unauthorized create post

--- spec/requests/api/v1/posts/create_post_spec.rb
+++ spec/requests/api/v1/posts/create_post_spec.rb
@@ -16,4 +16,15 @@ RSpec.describe '/POST communities/:id/posts', type: :request do
     expect(response.status).to eq(200)
     expect(Post.last.title).to eq(post_params.title)
   end
+
+  context 'when authorization is missing' do
+    it 'returns status 401 with errors' do
+      post_url = "/api/v1/communities/#{community.sub_dir}/posts"
+      post_params = build(:post)
+
+      post post_url, params: post_params, as: :json
+
+      expect(response.status).to eq(401)
+    end
+  end
 end
