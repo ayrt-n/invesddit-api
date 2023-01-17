f7937dcd3951bab954397a6fd12bf6111483c9cc
ayrton <ayrton.parkinson1@gmail.com>
2023-01-17 07:55:03 -0500

    Add test for creating post with invalid attributes

--- spec/requests/api/v1/posts/create_post_spec.rb
+++ spec/requests/api/v1/posts/create_post_spec.rb
@@ -27,4 +27,18 @@ RSpec.describe '/POST communities/:id/posts', type: :request do
       expect(response.status).to eq(401)
     end
   end
+
+  context 'when attributes invalid' do
+    it 'returns status 422 with errors' do
+      post_url = "/api/v1/communities/#{community.sub_dir}/posts"
+      post_params = build(:post, title: nil)
+
+      login_with_api(account)
+      post post_url, headers: {
+        Authorization: response['Authorization']
+      }, params: post_params, as: :json
+
+      expect(response.status).to eq(422)
+    end
+  end
 end
