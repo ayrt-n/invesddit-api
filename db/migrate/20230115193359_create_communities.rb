84dc0e7f2a82c61ef5d61dbc2d52e473673a6faf
ayrton <ayrton.parkinson1@gmail.com>
2023-01-17 07:09:34 -0500

    Add unique index to sub_dir

--- db/migrate/20230115193359_create_communities.rb
+++ db/migrate/20230115193359_create_communities.rb
@@ -1,7 +1,7 @@
 class CreateCommunities < ActiveRecord::Migration[7.0]
   def change
     create_table :communities do |t|
-      t.string :sub_dir
+      t.string :sub_dir, index: { unique: true }
       t.string :title
       t.text :description
 
