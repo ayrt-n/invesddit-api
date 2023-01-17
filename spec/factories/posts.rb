3296c89bda88a3e86d9780eed2da886ba40d3318
ayrton <ayrton.parkinson1@gmail.com>
2023-01-17 08:09:35 -0500

    Add associations to post factory

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :post do
+    # Post associations
+    association :account, status: :verified
+    association :community
+
+    # Post attributes to build
     title { Faker::Finance.stock_market }
     body { Faker::Lorem.paragraph }
   end
