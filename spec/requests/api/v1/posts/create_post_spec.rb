1e3006503e0c87625cd6e38b22290acfc8caa7a1
ayrton <ayrton.parkinson1@gmail.com>
2023-01-17 06:44:50 -0500

    Add test for successfully creating post

---
+++ spec/requests/api/v1/posts/create_post_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe '/POST communities/:id/posts', type: :request do
+  let(:account) { create(:account, :verified) }
+  let(:community) { create(:community) }
+
+  it 'creates a post' do
+    post_url = "/api/v1/communities/#{community.sub_dir}/"
+    post_params = build(:post)
+
+    login_with_api(account)
+    post post_url, headers: {
+      Authorization: response['Authorization']
+    }, params: post_params, as: :json
+
+    expect(response.status).to eq(200)
+  end
+end
