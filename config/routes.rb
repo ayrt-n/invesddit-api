2c7fac9f26ba4b645f448a051ea2d295834745a3
ayrton <ayrton.parkinson1@gmail.com>
2023-01-17 07:45:08 -0500

    Create nested create posts route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :communities, only: %i[index create show update]
+      resources :communities, only: %i[index create show update] do
+        resources :posts, only: %i[create]
+      end
     end
   end
 end
