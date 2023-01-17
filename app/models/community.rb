4b0a6295bbaa441ce29b0000ac8e165a54cf369a
ayrton <ayrton.parkinson1@gmail.com>
2023-01-17 07:44:54 -0500

    Add has_many posts

--- app/models/community.rb
+++ app/models/community.rb
@@ -2,6 +2,8 @@ class Community < ApplicationRecord
   extend FriendlyId
   friendly_id :sub_dir
 
+  has_many :posts
+
   validates :sub_dir, presence: true,
                       uniqueness: true,
                       length: { maximum: 20 }
