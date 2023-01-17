754476d03553a17f7bff34e04bcf46494830b91c
ayrton <ayrton.parkinson1@gmail.com>
2023-01-17 07:19:14 -0500

    Add shoulda-matchers config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -66,4 +66,12 @@ RSpec.configure do |config|
 
   # Add API helpers
   config.include ApiHelpers, type: :request
+
+  # Configure shoulda-matchers
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
 end
