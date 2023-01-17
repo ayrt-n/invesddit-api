d3dd7d4fffc07d53c7f73b7d06eb5c2b057af2c5
ayrton <ayrton.parkinson1@gmail.com>
2023-01-17 07:46:05 -0500

    Add test for creating post

--- spec/requests/api/v1/posts/create_post_spec.rb
+++ spec/requests/api/v1/posts/create_post_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe '/POST communities/:id/posts', type: :request do
   let(:community) { create(:community) }
 
   it 'creates a post' do
-    post_url = "/api/v1/communities/#{community.sub_dir}/"
+    post_url = "/api/v1/communities/#{community.sub_dir}/posts"
     post_params = build(:post)
 
     login_with_api(account)
@@ -14,5 +14,6 @@ RSpec.describe '/POST communities/:id/posts', type: :request do
     }, params: post_params, as: :json
 
     expect(response.status).to eq(200)
+    expect(Post.last.title).to eq(post_params.title)
   end
 end
