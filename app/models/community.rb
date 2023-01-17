04665ae3eacc6cc99698b818f8f604f7a0620077
ayrton <ayrton.parkinson1@gmail.com>
2023-01-17 07:19:30 -0500

    Add validations for sub_dir, title, description

--- app/models/community.rb
+++ app/models/community.rb
@@ -2,5 +2,10 @@ class Community < ApplicationRecord
   extend FriendlyId
   friendly_id :sub_dir
 
-  validates :sub_dir, presence: true, uniqueness: true
+  validates :sub_dir, presence: true,
+                      uniqueness: true,
+                      length: { maximum: 20 }
+
+  validates :title, length: { maximum: 20 }
+  validates :description, length: { maximum: 140 }
 end
