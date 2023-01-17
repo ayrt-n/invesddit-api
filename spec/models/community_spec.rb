a7255cb3c5e6273fa50f8cd0a1e118c920d36e17
ayrton <ayrton.parkinson1@gmail.com>
2023-01-17 07:19:40 -0500

    Add tests for validations

--- spec/models/community_spec.rb
+++ spec/models/community_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Community, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:sub_dir) }
+  it { is_expected.to validate_uniqueness_of(:sub_dir) }
+  it { is_expected.to validate_length_of(:sub_dir).is_at_most(20) }
+  it { is_expected.to validate_length_of(:title).is_at_most(20) }
+  it { is_expected.to validate_length_of(:description).is_at_most(140) }
 end
