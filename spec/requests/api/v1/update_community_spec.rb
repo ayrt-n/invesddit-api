9c6a9d0b14bc525f77e2d737bf39e4f96fe47352
ayrton <ayrton.parkinson1@gmail.com>
2023-01-17 06:24:01 -0500

    Add update Communities spec

---
+++ spec/requests/api/v1/update_community_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe '/PATCH communities', type: :request do
+  let(:account) { create(:account, :verified) }
+
+  it 'updates the community' do
+    community = create(:community, title: nil)
+    community_url = "/api/v1/communities/#{community.sub_dir}"
+
+    login_with_api(account)
+
+    patch community_url, headers: {
+      Authorization: response['Authorization']
+    }, params: { title: community.sub_dir }, as: :json
+
+    expect(response.status).to eq(200)
+    expect(json['title']).to eq(community.sub_dir)
+  end
+
+  context 'when authorization header is missing' do
+    it 'return status 401 with errors' do
+      community = create(:community, title: nil)
+      community_url = "/api/v1/communities/#{community.sub_dir}"
+
+      patch community_url, params: { title: community.sub_dir }, as: :json
+
+      expect(response.status).to eq(401)
+    end
+  end
+end
