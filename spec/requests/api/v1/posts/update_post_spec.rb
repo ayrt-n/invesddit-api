c149147015a0f281d7044d8e486939639b090520
ayrton <ayrton.parkinson1@gmail.com>
2023-01-18 07:11:14 -0500

    Add test for updating post

---
+++ spec/requests/api/v1/posts/update_post_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe '/PATCH posts/:id', type: :request do
+  it 'updates the post contents' do
+    account = create(:account, :verified)
+    post = create(:post, account: account)
+    post_url = "/api/v1/posts/#{post.id}"
+
+    login_with_api(account)
+    patch post_url, headers: {
+      Authorization: response['Authorization']
+    }, params: { post: { title: 'UPDATED POST' } }, as: :json
+
+    expect(response.status).to eq(200)
+    expect(Post.find(post.id).title).to eq('UPDATED POST')
+  end
+
+  context 'when authorization header missing' do
+    it 'returns status 401 and errors' do
+      post = create(:post)
+      post_url = "/api/v1/posts/#{post.id}"
+
+      patch post_url, params: { post: { title: 'UPDATED POST' } }, as: :json
+
+      expect(response.status).to eq(401)
+    end
+  end
+
+  context 'when request not made by post author' do
+    it 'returns status 401 with errors' do
+      account = create(:account, :verified)
+      post = create(:post)
+      post_url = "/api/v1/posts/#{post.id}"
+
+      login_with_api(account)
+      patch post_url, headers: {
+        Authorization: response['Authorization']
+      }, params: { post: { title: 'UPDATED POST' } }, as: :json
+
+      expect(response.status).to eq(401)
+    end
+  end
+end
