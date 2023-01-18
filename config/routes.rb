05d832cfad4a1971216b0f1fc9067e7ef889f6dc
ayrton <ayrton.parkinson1@gmail.com>
2023-01-18 07:10:51 -0500

    Add posts update route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
         resources :posts, only: %i[create]
       end
 
-      resources :posts, only: %i[index]
+      resources :posts, only: %i[index update]
     end
   end
 end
