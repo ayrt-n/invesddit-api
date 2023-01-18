d98cb03e631ca5664ad35938aa873f9145d3b683
ayrton <ayrton.parkinson1@gmail.com>
2023-01-18 07:22:59 -0500

    Add test for deleting post

---
+++ spec/requests/api/v1/posts/delete_post_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe '/DELETE posts/:id', type: :request do
+  it 'deletes the post' do
+    account = create(:account, :verified)
+    post = create(:post, account: account)
+    post_url = "/api/v1/posts/#{post.id}"
+
+    login_with_api(account)
+    delete post_url, headers: { Authorization: response['Authorization'] }, as: :json
+
+    expect(response.status).to eq(200)
+    expect(Post.exists?(post.id)).to eq(false)
+  end
+
+  context 'when authorization header is missing' do
+    it 'returns status 401 with errors' do
+      post = create(:post)
+      post_url = "/api/v1/posts/#{post.id}"
+
+      delete post_url, as: :json
+
+      expect(response.status).to eq(401)
+    end
+  end
+
+  context 'when request not made by post author' do
+    it 'returns status 401 with errors' do
+      account = create(:account, :verified)
+      post = create(:post)
+      post_url = "/api/v1/posts/#{post.id}"
+
+      login_with_api(account)
+      delete post_url, headers: { Authorization: response['Authorization'] }, as: :json
+
+      expect(response.status).to eq(401)
+    end
+  end
+end
