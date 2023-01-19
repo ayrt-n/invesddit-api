1126c2be934f6d27759b8c4780abfc98e965b19d
ayrton <ayrton.parkinson1@gmail.com>
2023-01-19 06:21:21 -0500

    Add polymorphic has_many for :commentable

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   belongs_to :account
 
+  has_many :comments, as: :commentable
+
   validates :body, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,7 @@ class Post < ApplicationRecord
   belongs_to :account
   belongs_to :community
 
+  has_many :comments, as: :commentable
+
   validates :title, presence: true
 end
