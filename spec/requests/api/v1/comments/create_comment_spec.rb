b5f953e2dd2c5e1ac70a8a559b512477c3393d00
ayrton <ayrton.parkinson1@gmail.com>
2023-01-19 06:25:31 -0500

    Add test for unauthorized create comment

--- spec/requests/api/v1/comments/create_comment_spec.rb
+++ spec/requests/api/v1/comments/create_comment_spec.rb
@@ -15,4 +15,17 @@ RSpec.describe '/POST /:commentable/:commentable_id/comments', type: :request do
     expect(response.status).to eq(200)
     expect(Comment.all.count).to eq(1)
   end
+
+  context 'when authorization header missing' do
+    it 'returns status 401 and errors' do
+      post = create(:post)
+      comment = build(:comment)
+      comments_url = "/api/v1/posts/#{post.id}/comments"
+
+      post comments_url, params: comment, as: :json
+
+      expect(response.status).to eq(401)
+      expect(Comment.all.count).to eq(0)
+    end
+  end
 end
