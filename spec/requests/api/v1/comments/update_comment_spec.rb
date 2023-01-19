cde784f90b58efc64640f1d54b5cb46367d8ffe8
ayrton <ayrton.parkinson1@gmail.com>
2023-01-19 06:51:39 -0500

    Add tests for updating comment

---
+++ spec/requests/api/v1/comments/update_comment_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe '/PATCH comments/:id', type: :request do
+  it 'updates the comment' do
+    account = create(:account, :verified)
+    comment = create(:comment, :for_post, account: account)
+    comment_url = "/api/v1/comments/#{comment.id}"
+
+    login_with_api(account)
+    patch comment_url, headers: {
+      Authorization: response['Authorization']
+    }, params: { comment: { body: 'UPDATED COMMENT' } }, as: :json
+
+    expect(response.status).to eq(200)
+    expect(Comment.find(comment.id).body).to eq('UPDATED COMMENT')
+  end
+
+  context 'when authorization header missing' do
+    it 'returns status 401 with errors' do
+      comment = create(:comment, :for_post)
+      comment_url = "/api/v1/comments/#{comment.id}"
+
+      patch comment_url, params: { comment: { body: 'UPDATED COMMENT' } }, as: :json
+
+      expect(response.status).to eq(401)
+    end
+  end
+
+  context 'when different user tries to update' do
+    it 'returns status 401 with errors' do
+      account = create(:account, :verified)
+      comment = create(:comment, :for_post)
+      comment_url = "/api/v1/comments/#{comment.id}"
+
+      login_with_api(account)
+      patch comment_url, headers: {
+        Authorization: response['Authorization']
+      }, params: { comment: { body: 'UPDATED COMMENT' } }, as: :json
+
+      expect(response.status).to eq(401)
+    end
+  end
+end
