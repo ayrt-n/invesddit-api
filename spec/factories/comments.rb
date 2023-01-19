cfe926b8ef6395f0eccf530b43a8f025bf2fb876
ayrton <ayrton.parkinson1@gmail.com>
2023-01-19 06:31:39 -0500

    Add associations to comment factory

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :comment do
     body { Faker::Lorem.sentence(word_count: 3) }
+    association :account, status: :verified
+
+    trait :for_post do
+      association :commentable, factory: :post
+    end
+
+    trait :for_comment do
+      association :commentable, factory: :comment
+    end
   end
 end
