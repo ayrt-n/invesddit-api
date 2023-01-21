ed1638bbac8864efa4ca082234728669a2383f74
ayrton <ayrton.parkinson1@gmail.com>
2023-01-21 13:24:10 -0500

    Set default account factory to create verified account

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -1,10 +1,19 @@
 FactoryBot.define do
   factory :account do
+    # Default accounts to be verified
+    verified
+
+    # Set fake email and password
     email { Faker::Internet.email }
     password { Faker::Internet.password }
-  end
 
-  trait :verified do
-    status { 'verified' }
+    # Traits to set account to verified/unverified
+    trait :verified do
+      status { 'verified' }
+    end
+
+    trait :unverified do
+      status { 'unverified' }
+    end
   end
 end
