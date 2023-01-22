4b98878491e61d54a93465fd855f7d1d1cb71b51
ayrton <ayrton.parkinson1@gmail.com>
2023-01-22 09:10:51 -0500

    Add test for non-admin trying to update community

--- spec/requests/api/v1/communities/update_community_spec.rb
+++ spec/requests/api/v1/communities/update_community_spec.rb
@@ -4,17 +4,36 @@ RSpec.describe '/PATCH communities', type: :request do
   let(:account) { create(:account, :verified) }
 
   it 'updates the community' do
+    # Create account and community
+    account = create(:account)
     community = create(:community, title: nil)
     community_url = "/api/v1/communities/#{community.sub_dir}"
 
-    login_with_api(account)
+    # Set account as admin of community
+    membership = create(:membership, :is_admin, account: account, community: community)
 
+    login_with_api(account)
     patch community_url, headers: {
       Authorization: response['Authorization']
     }, params: { title: community.sub_dir }, as: :json
 
     expect(response.status).to eq(200)
     expect(json['data']['title']).to eq(community.sub_dir)
+  end
+
+  context 'when non-admin tries to update' do
+    it 'returns status 401 with errors' do
+      account = create(:account)
+      community = create(:community, title: nil)
+      community_url = "/api/v1/communities/#{community.sub_dir}"
+
+      login_with_api(account)
+      patch community_url, headers: {
+        Authorization: response['Authorization']
+      }, params: { title: community.sub_dir }, as: :json
+
+      expect(response.status).to eq(401)
+    end
   end
 
   context 'when authorization header is missing' do
