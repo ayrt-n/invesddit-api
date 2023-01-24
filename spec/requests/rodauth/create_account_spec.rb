c651dc7d0f15c58d7ddbb6923696e80656eed017
ayrton <ayrton.parkinson1@gmail.com>
2023-01-24 07:33:49 -0500

    Update create account spec to include username

--- spec/requests/rodauth/create_account_spec.rb
+++ spec/requests/rodauth/create_account_spec.rb
@@ -7,19 +7,24 @@ RSpec.describe '/POST create-account', type: :request do
   it 'creates an account' do
     account_params = {
       login: account.email,
+      username: account.username,
       password: account.password,
       'password-confirm': account.password
     }
     post create_account_url, params: account_params, as: :json
 
+    record = Account.last
+
     expect(response.status).to eq(200)
-    expect(Account.last.email).to eq(account.email)
+    expect(record.email).to eq(account.email)
+    expect(record.username).to eq(account.username)
   end
 
   context 'when there are invalid attributes' do
     it 'returns status 422 with errors' do
       invalid_account_params = {
-        login: '',
+        login: account.email,
+        username: '',
         password: account.password,
         'password-confirm': account.password
       }
@@ -27,7 +32,6 @@ RSpec.describe '/POST create-account', type: :request do
       post create_account_url, params: invalid_account_params, as: :json
 
       expect(response.status).to eq(422)
-      # TO DO FORMAT AND CHECK ERRORS
     end
   end
 end
