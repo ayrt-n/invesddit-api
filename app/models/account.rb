e75315d8bf5b8f19b2eed4293c467d3028189754
ayrton <ayrton.parkinson1@gmail.com>
2023-01-24 07:49:42 -0500

    Add model level validations for username

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,6 +2,11 @@ class Account < ApplicationRecord
   include Rodauth::Rails.model
   enum :status, unverified: 1, verified: 2, closed: 3
 
+  validates :username, presence: true
+  validates :username, length: { in: 3..20 }
+  validates :username, format: { without: /\W/ }
+  validates :username, uniqueness: true
+
   has_many :memberships
   has_many :communities, through: :memberships
 end
