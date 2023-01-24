fd9b94c5d84ea3e757416b66454e30699e7cf818
ayrton <ayrton.parkinson1@gmail.com>
2023-01-24 06:32:27 -0500

    Add more post seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,12 @@ account3 = Account.create(email: 'fake3@test.com', password: 'pass1234')
 
 # Create community
 community1 = Community.create(sub_dir: 'KO')
+community2 = Community.create(sub_dir: 'SHOP')
 
 # Create posts
 post1 = Post.create(title: 'New post', body: 'new post', account: account1, community: community1)
+post2 = Post.create(title: 'New post', body: 'new post', account: account1, community: community1)
+post3 = Post.create(title: 'New post', body: 'new post', account: account1, community: community2)
 
 # Create votes
 post1.votes.create(vote: 1, account: account1)
