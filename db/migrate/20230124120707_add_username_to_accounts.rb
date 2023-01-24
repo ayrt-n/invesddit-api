e0c613d6069e2386399960532ce1879e06b3ba9f
ayrton <ayrton.parkinson1@gmail.com>
2023-01-24 07:49:30 -0500

    Add index to Accounts username

--- db/migrate/20230124120707_add_username_to_accounts.rb
+++ db/migrate/20230124120707_add_username_to_accounts.rb
@@ -1,5 +1,6 @@
 class AddUsernameToAccounts < ActiveRecord::Migration[7.0]
   def change
     add_column :accounts, :username, :string
+    add_index :accounts, :username
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -39,6 +39,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_24_120707) do
     t.string "password_hash"
     t.string "username"
     t.index ["email"], name: "index_accounts_on_email", unique: true, where: "(status = ANY (ARRAY[1, 2]))"
+    t.index ["username"], name: "index_accounts_on_username"
   end
 
   create_table "comments", force: :cascade do |t|
