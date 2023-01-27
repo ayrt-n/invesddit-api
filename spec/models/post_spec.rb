8c06374d4f160af35125de59d7b4de0085ce3c32
ayrton <ayrton.parkinson1@gmail.com>
2023-01-27 12:22:34 -0500

    Add test for #account_voted?

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -20,4 +20,22 @@ RSpec.describe Post, type: :model do
       expect(post.score).to eq(2)
     end
   end
+
+  context '#account_voted?' do
+    it 'returns false if the account has not voted' do
+      vote = create(:vote, :for_post)
+      post = vote.votable
+      other_account = create(:account)
+
+      expect(post.account_voted?(other_account)).to eq(false)
+    end
+
+    it 'returns true if the account has voted' do
+      account = create(:account)
+      vote = create(:vote, :for_post, account: account)
+      post = vote.votable
+
+      expect(post.account_voted?(account)).to eq(true)
+    end
+  end
 end
