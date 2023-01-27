c3d12d4f273af5c5568a2d042ab78ad78ef1a15f
ayrton <ayrton.parkinson1@gmail.com>
2023-01-27 12:22:47 -0500

    Add #account_voted?

--- app/models/concerns/votable.rb
+++ app/models/concerns/votable.rb
@@ -2,12 +2,18 @@ module Votable
   extend ActiveSupport::Concern
 
   included do
+    # Returns the score (upvotes minus downvotes)
     def score
       # Return zero if no votes on votable
       return 0 unless votes.exists?
 
       # If vote is upvote add one to score, if vote is downvote subtract one
       votes.reduce(0) { |score, vote| score + (vote.vote == 'upvote' ? 1 : -1) }
+    end
+
+    # Check if account has voted for votable and return bool
+    def account_voted?(account)
+      votes.pluck(:account_id).include?(account.id)
     end
   end
 end
