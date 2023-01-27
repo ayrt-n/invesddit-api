75d3c2b8212b7871833a772380cfc5a7dd9eea10
ayrton <ayrton.parkinson1@gmail.com>
2023-01-27 12:23:02 -0500

    Include Votable concern

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,9 @@ class Post < ApplicationRecord
   belongs_to :community
 
   has_many :comments, as: :commentable
+
   has_many :votes, as: :votable
+  include Votable
 
   validates :title, presence: true
 end
