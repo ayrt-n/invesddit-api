9fb67d3cd18540df95b086bf95bdb6a90d458fe9
ayrton <ayrton.parkinson1@gmail.com>
2023-01-27 09:57:11 -0500

    Add test for decrementing comment count on destroy

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Comment, type: :model do
   it { is_expected.to validate_presence_of(:body) }
 
   context 'after create' do
-    it 'updates Post comment count' do
+    it 'increments Post comment count' do
       # Create post and comments/nested comments
       post = create(:post)
       comment = create(:comment, commentable: post)
@@ -14,6 +14,17 @@ RSpec.describe Comment, type: :model do
       create(:comment, commentable: nested_comment)
 
       expect(post.comments_count).to eq(5)
+    end
+  end
+
+  context 'after destroy' do
+    it 'decrements Post comment count' do
+      post = create(:post)
+      comment = create(:comment, commentable: post)
+      create(:comment, commentable: post)
+      comment.destroy
+
+      expect(post.comments_count).to eq(1)
     end
   end
 end
