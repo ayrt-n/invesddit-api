4972535cd60997bb237264f4c81cb8623ad27b9a
ayrton <ayrton.parkinson1@gmail.com>
2023-01-27 09:58:02 -0500

    Add comments_count to Posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_24_125246) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_27_132412) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
@@ -81,6 +81,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_24_125246) do
     t.bigint "community_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "comments_count", default: 0, null: false
     t.index ["account_id"], name: "index_posts_on_account_id"
     t.index ["community_id"], name: "index_posts_on_community_id"
   end
