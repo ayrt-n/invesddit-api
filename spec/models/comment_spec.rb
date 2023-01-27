8c5ba16a0c1ef2703e311919d4288589f269e0d4
ayrton <ayrton.parkinson1@gmail.com>
2023-01-27 09:54:28 -0500

    Add test for incrementing comment count on creation

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   it { is_expected.to validate_presence_of(:body) }
+
+  context 'after create' do
+    it 'updates Post comment count' do
+      # Create post and comments/nested comments
+      post = create(:post)
+      comment = create(:comment, commentable: post)
+      nested_comment = create(:comment, commentable: comment)
+      create(:comment, commentable: post)
+      create(:comment, commentable: comment)
+      create(:comment, commentable: nested_comment)
+
+      expect(post.comments_count).to eq(5)
+    end
+  end
 end
