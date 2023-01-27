f6eab79040ed2630558806a7da3aefd543605a40
ayrton <ayrton.parkinson1@gmail.com>
2023-01-27 09:58:35 -0500

    Add after create and destroy callback to comments to increment/decrement Post comments count

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,4 +6,33 @@ class Comment < ApplicationRecord
   has_many :votes, as: :votable
 
   validates :body, presence: true
+
+  # Custom counter cache for Post model
+  after_create :increment_count
+  after_destroy :decrement_count
+
+  def increment_count
+    parent = find_root_post
+    # Increment the comment count
+    parent.increment!(:comments_count)
+  end
+
+  def decrement_count
+    parent = find_root_post
+    # Decrement the comment count
+    parent.decrement!(:comments_count)
+  end
+
+  private
+
+  # Polymorphic comment model allows comment to belong to either a post or a comment
+  # Method to find the root post a comment belongs to, returns the Post
+  def find_root_post
+    # Set parent to commentable
+    parent = commentable
+    # Continue moving up the associations until the parent is the root post
+    parent = parent.commentable until parent.is_a? Post
+
+    parent
+  end
 end
