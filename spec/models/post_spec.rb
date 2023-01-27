53022012d1fa2404afafa39681114329dcb1abcd
ayrton <ayrton.parkinson1@gmail.com>
2023-01-27 12:07:35 -0500

    Add test for #score

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,4 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   it { is_expected.to validate_presence_of(:title) }
+
+  context '#score' do
+    it 'has a default score of zero' do
+      post = create(:post)
+
+      expect(post.score).to eq(0)
+    end
+
+    it 'calculates the difference between upvotes and downvotes' do
+      post = create(:post)
+      create(:vote, votable: post, vote: 'upvote')
+      create(:vote, votable: post, vote: 'downvote')
+      create(:vote, votable: post, vote: 'upvote')
+      create(:vote, votable: post, vote: 'upvote')
+
+      expect(post.score).to eq(2)
+    end
+  end
 end
