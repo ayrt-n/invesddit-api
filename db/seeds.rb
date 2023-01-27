27fc991e775a9c372fcf0fa385b5c65f09495654
ayrton <ayrton.parkinson1@gmail.com>
2023-01-27 06:23:56 -0500

    Add additional seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # Create a few accounts
-account1 = Account.create(email: 'fake1@test.com', password: 'pass1234', username: 'finance_dude')
+account1 = Account.create(email: 'fake1@test.com', password: 'pass1234', username: 'finance_dude', status: :verified)
 account2 = Account.create(email: 'fake2@test.com', password: 'pass1234', username: 'ls_hedgie')
-account3 = Account.create(email: 'fake3@test.com', password: 'pass1234', username: 'bobby')
+account3 = Account.create(email: 'fake3@test.com', password: 'pass1234', username: 'bobby', status: :verified)
 
 # Create community
 community1 = Community.create(sub_dir: 'KO', description: 'Community dedicated to Coca Cola ($KO)! We like the drink and love the stock!')
@@ -11,6 +11,11 @@ community2 = Community.create(sub_dir: 'SHOP', description: 'For all things Shop
 post1 = Post.create(title: 'New post', body: 'new post', account: account1, community: community1)
 post2 = Post.create(title: 'New post', body: 'new post', account: account1, community: community1)
 post3 = Post.create(title: 'New post', body: 'new post', account: account1, community: community2)
+Post.create(title: 'New post', body: 'new post', account: account1, community: community2)
+Post.create(title: 'New post', body: 'new post', account: account2, community: community1)
+Post.create(title: 'New post', body: 'new post', account: account3, community: community2)
+Post.create(title: 'New post', body: 'new post', account: account2, community: community1)
+Post.create(title: 'New post', body: 'new post', account: account2, community: community2)
 
 # Create votes
 post1.votes.create(vote: 1, account: account1)
