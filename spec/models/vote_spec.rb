a52cee597eb22f29e3e03870df6a444da4cd1136
ayrton <ayrton.parkinson1@gmail.com>
2023-01-31 08:59:31 -0500

    Add tests for vote custom counter cache callbacks

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'after create' do
+    it 'updates the cached_score value of votable' do
+      upvote = create(:vote, type: 'upvote')
+      downvote = create(:vote, type: 'downvote')
+
+      expect(upvote.votable.cached_score).to eq(1)
+      expect(downvote.votable.cached_score).to eq(-1)
+    end
+  end
+
+  context 'after update' do
+    it 'updates the cached_score value of votable if changed' do
+      original_upvote = create(:vote, type: 'upvote')
+      original_downvote = create(:vote, type: 'downvote')
+      no_change_vote = create(:vote, type: 'upvote')
+
+      original_upvote.update_attribute(:type, 'downvote')
+      original_downvote.update_attribute(:type, 'upvote')
+      no_change_vote.update_attribute(:type, 'upvote')
+
+      expect(original_upvote.votable.cached_score).to eq(-1)
+      expect(original_downvote.votable.cached_score).to eq(1)
+      expect(no_change_vote.votable.cached_score).to eq(1)
+    end
+  end
+
+  context 'after destroy' do
+    it 'updates the cached_score value of votable if changed' do
+      upvote = create(:vote, type: 'upvote')
+      upvote.destroy
+
+      expect(upvote.votable.cached_score).to eq(0)
+    end
+  end
 end
