16270466da37e8311aed20442ccee16e2a5bac61
ayrton <ayrton.parkinson1@gmail.com>
2023-02-08 08:29:06 -0500

    Add uniquesnes constraint to account memberships

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,5 +2,7 @@ class Membership < ApplicationRecord
   belongs_to :community, counter_cache: true
   belongs_to :account
 
+  validates :account, uniqueness: { scope: [:community_id], message: 'is already a member' }
+
   enum :role, { member: 1, admin: 2 }
 end
