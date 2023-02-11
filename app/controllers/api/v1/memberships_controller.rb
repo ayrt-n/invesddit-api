2d987bfae3b4bb9391fc214197859316a79797b5
ayrton <ayrton.parkinson1@gmail.com>
2023-02-11 08:47:31 -0500

    Change current_account to @current_account

--- app/controllers/api/v1/memberships_controller.rb
+++ app/controllers/api/v1/memberships_controller.rb
@@ -1,11 +1,11 @@
 module Api
   module V1
     class MembershipsController < ApplicationController
-      before_action :authenticate, only: %i[create]
+      before_action :authenticate, only: %i[create destroy]
 
       def create
         @community = Community.friendly.find(params[:community_id])
-        @membership = @community.memberships.build(account_id: current_account.id)
+        @membership = @community.memberships.build(account_id: @current_account.id)
 
         if @membership.save
           render_resource(@membership)
@@ -16,7 +16,7 @@ module Api
 
       def destroy
         @membership = Membership.find(params[:id])
-        return access_denied unless @membership.account == current_account
+        return access_denied unless @membership.account == @current_account
 
         if @membership.destroy
           head :no_content
