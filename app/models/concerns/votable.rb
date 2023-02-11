8eae837d14e1bdcc5c63ac028b34244439f7d612
ayrton <ayrton.parkinson1@gmail.com>
2023-02-11 10:57:07 -0500

    Add #vote_type_by_account

--- app/models/concerns/votable.rb
+++ app/models/concerns/votable.rb
@@ -6,5 +6,9 @@ module Votable
     def account_voted?(account)
       votes.pluck(:account_id).include?(account.id)
     end
+
+    def vote_type_by_account(account)
+      votes.find_by(account:)&.vote_type
+    end
   end
 end
