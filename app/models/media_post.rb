f8f5bc5fca19ad30c129153e95d26a3b8130cf0a
ayrton <ayrton.parkinson1@gmail.com>
2023-02-18 08:47:16 -0500

    Add validation for presence of image

--- app/models/media_post.rb
+++ app/models/media_post.rb
@@ -1,2 +1,4 @@
 class MediaPost < Post
+  has_one_attached :image
+  validates :image, presence: true
 end
--- spec/models/media_post_spec.rb
+++ spec/models/media_post_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe MediaPost, type: :model do
   it_behaves_like 'Post'
+  it { is_expected.to validate_presence_of(:image) }
 end
