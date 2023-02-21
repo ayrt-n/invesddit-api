6c20995787935d1c3b6d09b1d078d480cae30da9
ayrton <ayrton.parkinson1@gmail.com>
2023-02-21 07:24:19 -0500

    Set default format to json

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,23 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      resources :communities, only: %i[index create show update] do
-        resources :text_posts, controller: 'posts', only: %i[create], type: 'TextPost'
-        resources :link_posts, controller: 'posts', only: %i[create], type: 'LinkPost'
-        resources :media_posts, controller: 'posts', only: %i[create], type: 'MediaPost'
-        resource :memberships, only: %i[create destroy]
-      end
+  defaults format: :json do
+    namespace :api do
+      namespace :v1 do
+        resources :communities, only: %i[index create show update] do
+          resources :text_posts, controller: 'posts', only: %i[create], type: 'TextPost'
+          resources :link_posts, controller: 'posts', only: %i[create], type: 'LinkPost'
+          resources :media_posts, controller: 'posts', only: %i[create], type: 'MediaPost'
+          resource :memberships, only: %i[create destroy]
+        end
 
-      resources :posts, only: %i[index show update destroy] do
-        resources :comments, only: %i[index create]
-        resource :votes, only: %i[create destroy]
-      end
+        resources :posts, only: %i[index show update destroy] do
+          resources :comments, only: %i[index create]
+          resource :votes, only: %i[create destroy]
+        end
 
-      resources :comments, only: %i[update] do
-        resource :comments, only: %i[create]
-        resource :votes, only: %i[create destroy]
+        resources :comments, only: %i[update] do
+          resource :comments, only: %i[create]
+          resource :votes, only: %i[create destroy]
+        end
       end
     end
   end
