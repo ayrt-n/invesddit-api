af9bb25b299069d7f4d380db155980708e5fb4ca
ayrton <ayrton.parkinson1@gmail.com>
2023-03-26 12:05:24 -0400

    Update cors policy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,7 +13,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       origins 'http://ayrt-n.github.io'
     end
 
-    resource '*',
+    resource '/*',
       headers: :any,
       methods: %i[get post put patch delete options head],
       expose: ['Authorization']
