37e89c2985cb222c462af2e5bbb4a2f9ce80d9cc
ayrton <ayrton.parkinson1@gmail.com>
2023-03-26 10:19:18 -0400

    Fix default prod mailer url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
-  config.action_mailer.default_url_options = { host: 'https://ayrt-n.github.io/invesddit/' }
+  config.action_mailer.default_url_options = { host: 'ayrt-n.github.io/invesddit' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
