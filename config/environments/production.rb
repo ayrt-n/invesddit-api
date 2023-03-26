b2267962ae0211085219ac612e845127683a39fa
ayrton <ayrton.parkinson1@gmail.com>
2023-03-26 09:59:14 -0400

    Add sendgrid mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,21 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Action mailer production settings
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    user_name: 'apikey',
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'ayrt-n.github.io/invesddit/',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
+  config.action_mailer.default_url_options = { host: 'https://ayrt-n.github.io/invesddit/' }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
