435258f93c5e0427c59ff4374ad628da0b1e06fc
ayrton <ayrton.parkinson1@gmail.com>
2023-03-29 07:05:25 -0400

    Remove duplicate test

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Account, type: :model do
   it { is_expected.to validate_presence_of(:username) }
   it { is_expected.to validate_length_of(:username).is_at_least(3).is_at_most(20) }
-  it { is_expected.to validate_presence_of(:username) }
 
   context '#join_community' do
     it 'creates membership for account and community' do
