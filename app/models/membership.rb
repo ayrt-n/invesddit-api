4c3a08da53bc7e5195d4c78db478b5cb254538e0
ayrton <ayrton.parkinson1@gmail.com>
2023-04-09 09:44:04 -0400

    Add validation for uniqueness of account - communtiy - role

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,7 +2,13 @@ class Membership < ApplicationRecord
   belongs_to :community, counter_cache: true
   belongs_to :account
 
-  validates :account, uniqueness: { scope: [:community_id], message: 'is already a member' }
+  validates :account,
+            uniqueness: {
+              scope: %i[community_id role],
+              message: lambda { |object, _data|
+                object.role == 'member' ? 'is already a member' : 'is already an admin'
+              }
+            }
 
   enum :role, { member: 1, admin: 2 }
 end
