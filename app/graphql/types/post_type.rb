b8d63ef3ff3a042212a48e55eeb200ff4a259846
ayrton <ayrton.parkinson1@gmail.com>
2024-12-01 08:50:55 -0500

    Add field descriptions

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -1,27 +1,29 @@
 module Types
   class PostType < Types::BaseObject
     # Scalar fields
-    field :id, ID, null: false
-    field :title, String, null: false
-    field :body, String, null: true
-    field :account_id, Integer, null: false
-    field :community_id, Integer, null: false
-    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :comments_count, Integer, null: false
-    field :cached_score, Integer, null: false
-    field :cached_hot_rank, Float, null: true
-    field :cached_upvotes, Integer, null: true
-    field :cached_downvotes, Integer, null: true
-    field :cached_confidence_score, Float, null: true
-    field :type, String, null: false
+    field :id, ID, null: false, description: 'The id of the post'
+    field :title, String, null: false, description: 'The title of the post'
+    field :body, String, null: true, description: 'The body of the post'
+    field :account_id, Integer, null: false, description: 'The account id of the author of the post'
+    field :community_id, Integer, null: false, description: 'The community id of the community the post belongs to'
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false, description: 'The datetime the post was created'
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false,
+                                                        description: 'The datetime the post was last updated'
+    field :comments_count, Integer, null: false, description: 'The number of comments on the post'
+    field :cached_score, Integer, null: false, description: 'The score (upvotes - downvotes) of the post'
+    field :cached_hot_rank, Float, null: true, description: 'The hot ranking of the post'
+    field :cached_upvotes, Integer, null: true, description: 'The number of upvotes on the post'
+    field :cached_downvotes, Integer, null: true, description: 'The number of downvotes on the post'
+    field :cached_confidence_score, Float, null: true, description: 'The confidence score of the post'
+    field :type, String, null: false, description: 'The type of post (TEXT, MEDIA, LINK)'
 
     # Enum fields
-    field :status, Types::StatusEnumType, null: false
+    field :status, Types::StatusEnumType, null: false, description: 'The status of the post (PUBLISHED or DELETED)'
 
     # Association fields
-    field :account, Types::AccountType, null: false
-    field :community, Types::CommunityType, null: false
-    field :comments, [Types::CommentType], null: false, method: :top_level_comments
+    field :account, Types::AccountType, null: false, description: 'The author of the post'
+    field :community, Types::CommunityType, null: false, description: 'The community the post belongs to'
+    field :comments, [Types::CommentType], null: false, method: :top_level_comments,
+                                           description: 'Top-level comments on the post'
   end
 end
